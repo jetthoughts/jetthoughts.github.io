39e26a8b45c9227536f09ad07de336580fa00ea1
Paul Keen <125715+pftg@users.noreply.github.com>
2024-09-15 00:32:50 +0200

    refact: removes more redundant classes and js code

--- postcss.config.js
+++ postcss.config.js
@@ -14,19 +14,31 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
       "pp-off-canvas-menu-module", "pp-off-canvas-menu-open", "pp-full-screen-menu-module",
       "pp-full-screen-menu-open", "fl-node-menu-clone", "pp-search-form--focus", "first-click",
 
-      "pp-advanced-menu", "pp-advanced-menu-accordion-collapse", "pp-menu-default","pp-menu-align-right",
-      "pp-menu-position-below", "fl-module-content","fl-node-content",
+      "pp-advanced-menu", "pp-advanced-menu-accordion-collapse", "pp-menu-default", "pp-menu-align-right",
+      "pp-menu-position-below", "fl-module-content", "fl-node-content",
 
       // pp adv tabs
       "pp-tabs", "pp-tabs-label", "pp-tab-active", "pp-tabs-panel", "pp-tabs-panel-content",
       "pp-tabs-animation", "pp-no-border",
+
       // fl-theme
       "collapse", "fl-mobile-sub-menu-open", "current-menu-item", "fl-sub-menu-right", "fl-shrink-header-enabled",
-      "fl-show", "mega-menu-disabled", "mega-menu", /^mega-menu-items-/, "fl-responsive-nav-enabled", "fl-nav-offcanvas-collapse",
+      "fl-show", "mega-menu-disabled", "mega-menu", /^mega-menu-items-/, "fl-responsive-nav-enabled",
+      "fl-nav-offcanvas-collapse",
+
+      // pp google form
+      // /^pp-gf/, "fl-button", "pp-button",
+
+      // sdb
+      "jt-active",
+    ],
+
+    deep: [
+      /^fl-page/
     ],
 
     greedy: [
-      /^swiper-/, /^is-/, /^has-/, /^js-/, /^fl-builder-content/, /^fl-col/, /^fl-node/
+      /^swiper-/, /^is-/, /^has-/, /^js-/, /^fl-builder-content/, /^fl-col/, /^fl-node/,
     ]
   },
 })
@@ -34,8 +46,9 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 module.exports = {
   plugins: [
     require("postcss-nested"),
+    require("postcss-delete-duplicate-css")({ isRemoveNull: true, isRemoveComment: true }),
     purgecss,
-    require("postcss-delete-duplicate-css")({isRemoveNull: true, isRemoveComment: true}),
+    require("postcss-delete-duplicate-css")({ isRemoveNull: true, isRemoveComment: true }),
     ...(process.env.HUGO_ENVIRONMENT === "production" ? [require("autoprefixer"), require("cssnano")] : []),
   ],
 }
