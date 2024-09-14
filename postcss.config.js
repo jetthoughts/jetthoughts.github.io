15d7b97e5103ef6baec2ba2af7238663cfa81c24
Paul Keen <125715+pftg@users.noreply.github.com>
2024-09-14 22:57:05 +0200

    minify only for production

--- postcss.config.js
+++ postcss.config.js
@@ -14,8 +14,8 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 module.exports = {
   plugins: [
     require("postcss-nested"),
+    purgecss,
     require("postcss-delete-duplicate-css")({isRemoveNull: true, isRemoveComment: true}),
-    purgecss, require("autoprefixer"), require("cssnano"),
     ...(process.env.HUGO_ENVIRONMENT === "production" ? [require("autoprefixer"), require("cssnano")] : []),
   ],
 }
