6cb0f71ac32db93d397773303c33f16da0049bc2
Andriy Parashchuk <anrey.andreevich@gmail.com>
2024-09-16 13:13:25 +0300

    #53: Fix mobile menu script

--- themes/beaver/assets/js/navigation.js
+++ themes/beaver/assets/js/navigation.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-   document.querySelectorAll('.js-mobile-menu-opener, .js-mobile-menu-close').addEventListener('click', function (e) {
-    document.querySelector('.js-navigation, body').toggle('-open');
+  document.querySelector('.js-mobile-menu-opener').addEventListener('click', function (e) {
+    document.querySelector('.js-navigation').classList.add('-open');
+    document.querySelector('body').classList.add('-open');
+  }, false);
+
+  document.querySelector('.js-mobile-menu-close').addEventListener('click', function (e) {
+    document.querySelector('.js-navigation').classList.remove('-open');
+    document.querySelector('body').classList.remove('-open');
   }, false);
 
   if (document.querySelector(window).width() < 1024) {
-    document.querySelectorAll('.js-sub-menu-opener').addEventListener('click', function (e) {
-      document.querySelector(this).parent().toggle('-open');
+    document.querySelector('.js-sub-menu-opener').addEventListener('click', function (e) {
+      document.querySelector(this).parent().classList.toggle('-open');
     })
   }
 });
