602c51d9e0c81b75e3f3c8dc7cb8628e80245e0b
AucT <auct.eu@gmail.com>
2024-10-08 11:20:37 +0300

    Redo technologies block with grid

--- postcss.config.js
+++ postcss.config.js
@@ -30,7 +30,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
     ],
 
     greedy: [
-      /^swiper-/, /^is-/, /^has-/, /^js-/, /^fl-builder-content/, /^fl-col/, /^fl-node/,
+      /^swiper-/, /^is-/, /^has-/, /^js-/, /^fl-builder-content/, /^fl-col/, /^fl-node/, /^technologies/
     ]
   },
 })
