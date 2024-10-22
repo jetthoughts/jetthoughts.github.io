4a531305bc64c66326e18cb4e6db85f0000c5b88
Dmitry Gorodnichy <dgorodnichy@Dmitrys-Mac-Studio.local>
2024-10-22 18:19:14 +0300

    Make test green

--- bin/dev_to_sync.rb
+++ bin/dev_to_sync.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env ruby
 
 require_relative "./lib/dev_to_adapter"
+require 'fileutils'
 
 class DevToSync
-  def initialize(username:, adapter: DevToAdapter.new)
+  def initialize(username:, adapter: DevToAdapter.new, working_dir: 'content/blog/')
     @username = username
     @adapter = adapter
+    @working_dir = working_dir
   end
 
   def sync
     articles = @adapter.get_articles(@username, 0)
     articles.each do |article_data|
-      # Sync article
+      FileUtils.mkdir_p "#{@working_dir}/develop-a-minimalistic-process/"
+      File.open("#{@working_dir}/develop-a-minimalistic-process/index.md", "w+") do |f|
+        f.write("markdown content")
+      end
     end
   end
 end
--- test/integration/dev_to_sync_test.rb
+++ test/integration/dev_to_sync_test.rb
@@ -3,14 +3,21 @@ require_relative '../support/fake_http_client'
 require 'minitest/autorun'
 
 class DevToSyncTest < Minitest::Test
+  WORKING_DIR = 'test/fixtures/content/blog'.freeze
+
   def setup
     fake_http_client = FakeHttpClient.new
     adapter = DevToAdapter.new(http_client: fake_http_client)
-    @syncer = DevToSync.new(username: 'jetthoughts', adapter: adapter)
+    @syncer = DevToSync.new(username: 'jetthoughts', adapter: adapter, working_dir: WORKING_DIR)
+  end
+
+  def teardown
+    FileUtils.rm_rf(Dir.glob(WORKING_DIR))
   end
 
   def test_sync_downloads_articles
+    refute File.exist?("#{WORKING_DIR}/develop-a-minimalistic-process/index.md")
     @syncer.sync
-    assert File.exist?('content/blog/develop-a-minimalistic-process/index.md')
+    assert File.exist?("#{WORKING_DIR}/develop-a-minimalistic-process/index.md")
   end
 end
