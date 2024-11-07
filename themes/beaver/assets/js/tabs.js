27e4b031f94fa7e5fef4b6a865f7e5029b4bb266
AucT <auct.eu@gmail.com>
2024-11-07 10:15:04 +0200

    add keyboard navigation

--- themes/beaver/assets/js/tabs.js
+++ themes/beaver/assets/js/tabs.js
@@ -13,3 +13,34 @@ document.querySelectorAll('.js-tab').forEach(el => {
         }
     }
 });
+
+//for keyboard
+document.querySelectorAll('.tabs').forEach(tabList => {
+    const tabs = tabList.querySelectorAll('.js-tab');
+    let tabFocus = 0;
+    tabList.addEventListener("keydown", (e) => {
+        // Move right
+        if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === 'Enter' || e.key === ' ') {
+            tabs[tabFocus].setAttribute("tabindex", -1);
+            if (e.key === "ArrowDown") {
+                tabFocus++;
+                // If we're at the end, go to the start
+                if (tabFocus >= tabs.length) {
+                    tabFocus = 0;
+                }
+                // Move left
+            } else if (e.key === "ArrowUp") {
+                tabFocus--;
+                // If we're at the start, move to the end
+                if (tabFocus < 0) {
+                    tabFocus = tabs.length - 1;
+                }
+            } else if (e.key === 'Enter' || e.key === ' ') {
+                tabs[tabFocus].click();
+            }
+
+            tabs[tabFocus].setAttribute("tabindex", 0);
+            tabs[tabFocus].focus();
+        }
+    });
+});
