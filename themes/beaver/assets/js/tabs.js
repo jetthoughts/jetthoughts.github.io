1c1644c66822099aca192b11db65098751a6c66e
AucT <auct.eu@gmail.com>
2024-11-07 10:14:52 +0200

    change aria-selected on tab select

--- themes/beaver/assets/js/tabs.js
+++ themes/beaver/assets/js/tabs.js
@@ -1,11 +1,14 @@
 document.querySelectorAll('.js-tab').forEach(el => {
     const container = el.closest('.js-tabs-container');
     el.onclick = function (e) {
-        const panel = document.getElementById(e.target.getAttribute('aria-controls'));
+        const targetTab = e.target;
+        const panel = document.getElementById(targetTab.getAttribute('aria-controls'));
         container.querySelectorAll(".tab-panel").forEach(panel => panel.hidden = true);
         panel.hidden = false;
+        container.querySelectorAll(".js-tab").forEach(tabTitle => tabTitle.setAttribute('aria-selected', 'false'));
+        targetTab.setAttribute('aria-selected', 'true');
         if (window.innerWidth < 860) {
-            panel.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            panel.scrollIntoView({behavior: 'smooth', block: 'center'});
             panel.focus();
         }
     }
