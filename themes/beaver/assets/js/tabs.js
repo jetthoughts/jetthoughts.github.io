8631844219d10cd7638544c380e6566986e64002
AucT <auct.eu@gmail.com>
2024-11-12 14:29:35 +0200

    refactor use cases html css js (#200)
    * refactor use cases html css js
    
    * change aria-selected on tab select
    
    * add keyboard navigation
    
    * add preventDefault for keyboard navigation
    
    * remove not needed !important
    
    * rename d-flex, flex1
    remove important
    
    * format using 2 spaces
    
    * remove utility class usage
    
    * add new line
    
    * merge duplicate
    
    * add newline
    
    * show background only for hover, focus, selected tab
    
    * prevent default only for arrows, enter
    
    * use boolean in dict
    
    * add mobile tabs
    
    * test: upds scrns
    
    * build: use new ruby
    
    ---------
    
    Co-authored-by: Paul Keen <125715+pftg@users.noreply.github.com>

---
+++ themes/beaver/assets/js/tabs.js
@@ -0,0 +1,46 @@
+document.querySelectorAll('.js-tab').forEach(el => {
+    const container = el.closest('.js-tabs-container');
+    el.onclick = function (e) {
+        const targetTab = e.target;
+        const panel = document.getElementById(targetTab.getAttribute('aria-controls'));
+        container.querySelectorAll(".tab-panel").forEach(panel => panel.hidden = true);
+        panel.hidden = false;
+        container.querySelectorAll(".js-tab").forEach(tabTitle => tabTitle.setAttribute('aria-selected', 'false'));
+        targetTab.setAttribute('aria-selected', 'true');
+        if (window.innerWidth < 860) {
+            panel.scrollIntoView({behavior: 'smooth', block: 'center'});
+            panel.focus();
+        }
+    }
+});
+
+//for keyboard
+document.querySelectorAll('.tabs').forEach(tabList => {
+    const tabs = tabList.querySelectorAll('.js-tab');
+    let tabFocus = 0;
+    tabList.addEventListener("keydown", (e) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            tabs[tabFocus].setAttribute("tabindex", -1);
+            if (e.key === "ArrowDown") {
+                tabFocus++;
+                // If we're at the end, go to the start
+                if (tabFocus >= tabs.length) {
+                    tabFocus = 0;
+                }
+                // Move left
+            } else if (e.key === "ArrowUp") {
+                tabFocus--;
+                // If we're at the start, move to the end
+                if (tabFocus < 0) {
+                    tabFocus = tabs.length - 1;
+                }
+            } else if (e.key === 'Enter' || e.key === ' ') {
+                tabs[tabFocus].click();
+            }
+
+            tabs[tabFocus].setAttribute("tabindex", 0);
+            tabs[tabFocus].focus();
+        }
+    });
+});
